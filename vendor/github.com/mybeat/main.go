d20fa5c4e72379d5da872fb81fd804aa754b002b
Enzo <enzohx@163.com>
2018-09-29 14:42:47 +0800

    govender add +all

---
+++ vendor/github.com/mybeat/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/mybeat/beater"
+)
+
+func main() {
+	err := beat.Run("mybeat", "", beater.New)
+	if err != nil {
+		os.Exit(1)
+	}
+}
